fd79085d7a5bbf0ea5157ad4f6f061bfb8ae8cc0
keitzer <ogorek@usc.edu>
2015-11-14 15:19:56 -0500

    Changes.

--- TrainWithMe/ViewController.m
+++ TrainWithMe/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.//whatever
+	// Do any additional setup after loading the view, typically from a nib.//whatever//change.
 }
 
 - (void)didReceiveMemoryWarning {
